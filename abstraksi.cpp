abd64d4ef2bce47c66a656294b67b06f96e3bbfb
Mayzihardiputra <eziee496@gmail.com>
2025-06-02 18:14:45 +0700

    public

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -5,4 +5,13 @@ class AbstraksiKlas
 {
 private:
     string x, y;
+
+public:
+    // menthod untuk mengisi nilai
+    // private number
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
 };
