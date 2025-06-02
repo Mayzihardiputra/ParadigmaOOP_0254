493d2729546fe6817879f069b8dabf795ba36c2b
Mayzihardiputra <eziee496@gmail.com>
2025-06-02 18:51:19 +0700

    no0verring

---
+++ no0verriding.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class baseClass
+{
+public:
+    virtual void perkenalan() final
+    {
+        cout << "Halo saya Function dari base class";
+    }
+};
+
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "hallo saya function dari derived class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
